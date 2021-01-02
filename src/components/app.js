54ec08612a248e815103a87849fed2915c6c21c8
scottdavenport <scott@pinpoint.com>
2021-01-02 13:21:18 -0500

    Showing Forms to the User

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './SearchBar';
 
 const App = () => {
-    return <div>App</div>;
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
 };
 
 export default App;
