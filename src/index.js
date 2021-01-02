e54b31b02df0541ed11edd7b8b9a80a32b1697fd
scottdavenport <scott@pinpoint.com>
2021-01-02 10:45:29 -0500

    Setup index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+
+    render() {
+
+    }
+}
+
+
+ReactDOM.render(<App />, document.querySelector('#root'));
