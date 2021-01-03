0caa1d4571556a93d67fc73e527ba24a2a8dc5a2
scottdavenport <scott@pinpoint.com>
2021-01-03 10:31:55 -0500

    Handling Requests with Async Await - .then promise

--- src/components/app.js
+++ src/components/app.js
@@ -4,12 +4,17 @@ import SearchBar from './SearchBar';
 
 class App extends React.Component {
 	onSearchSubmit(term) {
-		axios.get('https://api.unsplash.com/search/photos', {
-			params: { query: term },
-			headers: {
-				Authorization: 'Client-ID UQ_EwqCZkQp43VBg9ZtmV88FvVXUFTgKDuCX2MM3azE',
-			},
-		});
+		axios
+			.get('https://api.unsplash.com/search/photos', {
+				params: { query: term },
+				headers: {
+					Authorization: 'Client-ID UQ_EwqCZkQp43VBg9ZtmV88FvVXUFTgKDuCX2MM3azE',
+				},
+				// callback that will be invoked when we receive data from unsplash
+			})
+			.then((response) => {
+				console.log(response.data.results);
+			});
 	}
 
 	render() {
