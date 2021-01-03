1106e1a5dada797b1593ea769ac08ae593129db6
scottdavenport <scott@pinpoint.com>
2021-01-03 17:51:20 -0500

    Accessing Image Height...watch for timing issue

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.imageRef = React.createRef();
+	}
+
+	componentDidMount() {
+		console.log(this.imageRef);
+		// we see 0 for the 'clientHeight' because it's called too quickly before the image is downloaded.
+		console.log(this.imageRef.current.clientHeight);
+	}
+
 	render() {
 		const { description, urls } = this.props.image;
 
 		return (
 			<div>
-				<img alt={description} src={urls.regular} />
+				<img ref={this.imageRef} alt={description} src={urls.regular} />
 			</div>
 		);
 	}
